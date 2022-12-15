af3b5134b37384aed36df64c2c7eb3450fb4741c
Konstantin Myrsaitov <myrsaitov@gmail.com>
2022-12-15 20:48:25 +0300

    Переконфигурирование проекта

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BoardTopBarAccountComponent } from './app-components/board-top-bar-account/board-top-bar-account.component';
-import { BoardTopBarMainMenuComponent } from './app-components/board-top-bar-main-menu/board-top-bar-main-menu.component';
-import { BoardTopBarSearchComponent } from './app-components/board-top-bar-search/board-top-bar-search.component';
-import { BoardLeftColumnCategoriesComponent } from './app-components/board-left-column-categories/board-left-column-categories.component';
-import { BoardRightColumnTagsComponent } from './app-components/board-right-column-tags/board-right-column-tags.component';
-import { BoardRouterPageDashboardComponent } from './app-components/board-router-page-dashboard/board-router-page-dashboard.component';
-import { BoardRouterPageViewComponent } from './app-components/board-router-page-view/board-router-page-view.component';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { ru_RU } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import ru from '@angular/common/locales/ru';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IconsProviderModule } from './icons-provider.module';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
@@ -25,21 +18,14 @@ registerLocaleData(ru);
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BoardTopBarAccountComponent,
-    BoardTopBarMainMenuComponent,
-    BoardTopBarSearchComponent,
-    BoardLeftColumnCategoriesComponent,
-    BoardRightColumnTagsComponent,
-    BoardRouterPageDashboardComponent,
-    BoardRouterPageViewComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
+    BrowserAnimationsModule,
     IconsProviderModule,
     NzLayoutModule,
     NzMenuModule
